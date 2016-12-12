b6b31dd7d68b462f460b533ffbea24dcd6aaf09b
Piotr Jakubik <wiatrogon@gmail.com>
2016-12-12 13:18:20 +0100

    tests

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -7,12 +7,12 @@ use PHPUnit\Framework\TestCase;
 
 class ApiTest extends TestCase
 {
+
     function testGetUri()
     {
         $api = new Allegro\REST\Api('eggs', 'spam', 'ham', 'beans');
 
-        $this->assertEquals('https://allegroapi.io/',
-                            $api->getUri());
+        $this->assertEquals('https://allegroapi.io/', $api->getUri());
 
         $this->assertEquals('https://allegroapi.io/categories/',
                             $api->categories->getUri());
@@ -21,12 +21,36 @@ class ApiTest extends TestCase
                             $api->categories(12)->getUri());
     }
 
-    function testGetAutorizationUri()
+    /**
+     * @dataProvider credentialsProvider
+     */
+    function testAuthorization($clientId, $clientSecret, $apiKey,
+                               $redirectUri, $accessToken, $refreshToken)
     {
-        $api = new Allegro\REST\Api('eggs', 'spam', 'ham', 'beans');
+        $api = new Allegro\REST\Api($clientId, $clientSecret, $apiKey,
+                                    $redirectUri, $accessToken, $refreshToken);
 
-        $this->assertEquals('https://ssl.allegro.pl/auth/oauth/authorize' .
-                            '?response_type=code&client_id=eggs&api-key=ham&redirect_uri=beans',
-                            $api->getAuthorizationUri());
+        $expected = 'https://ssl.allegro.pl/auth/oauth/authorize' .
+                    "?response_type=code&client_id=$clientId" .
+                    "&api-key=$apiKey&redirect_uri=$redirectUri";
+
+        $this->assertEquals($expected, $api->getAuthorizationUri());
+
+        $this->assertEquals($accessToken, $api->getAccessToken());
+        $this->assertEquals($accessToken, $api->categories->getAccessToken());
+        $this->assertEquals($accessToken, $api->categories(123)->getAccessToken());
+
+        $this->assertEquals($apiKey, $api->getApiKey());
+        $this->assertEquals($apiKey, $api->categories->getApiKey());
+        $this->assertEquals($apiKey, $api->categories(123)->getApiKey());
+    }
+
+    function credentialsProvider()
+    {
+        return array(
+            array('eggs', 'spam', 'ham', 'beans', null, null),
+            array('wood', 'stone', 'clay', 'wool', 'wheat', 'depleted uranium'),
+            array('white', 'blue', 'black', 'red', 'green', 'colorless')
+        );
     }
 }
