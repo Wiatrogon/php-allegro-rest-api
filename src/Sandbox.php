8c0fc87fe2b70ebec8ec3fffc118ca2a91efa6d4
Paweł Bukowski <pbukowski@telaxus.com>
2017-11-16 09:00:53 +0100

    fix uri - remove trailing /

--- src/Sandbox.php
+++ src/Sandbox.php
@@ -4,7 +4,7 @@ namespace Allegro\REST;
 class Sandbox extends Api
 {
 
-    const API_URI = 'https://sandbox.allegroapi.io/';
+    const API_URI = 'https://sandbox.allegroapi.io';
 
     const TOKEN_URI = 'https://ssl.allegro.pl.webapisandbox.pl/auth/oauth/token';
 
